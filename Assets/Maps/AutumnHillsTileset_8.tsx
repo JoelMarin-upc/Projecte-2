1d54fb5ed9d50f9a65360e00d5cef6a04e18a59d
sofia-221b <sofia.bar.nav@gmail.com>
2026-03-03 12:19:48 +0100

    RebelRefugeMap

---
+++ Assets/Maps/AutumnHillsTileset_8.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="AutumnHillsTileset_8" tilewidth="20" tileheight="20" tilecount="625" columns="25">
+ <image source="AutumnHillsTileset_8.png" width="500" height="500"/>
+</tileset>
