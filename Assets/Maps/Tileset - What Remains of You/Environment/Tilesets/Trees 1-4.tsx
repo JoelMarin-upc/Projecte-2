141b30ada953137316852df82183b3cfe2a5dc47
Valkyn22 <clara.sanchez.via@gmail.com>
2026-04-01 15:52:23 +0200

    change in the format

--- Assets/Maps/Tileset - What Remains of You/Environment/Tilesets/Trees 1-4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Trees 1-4" tilewidth="16" tileheight="16" tilecount="368" columns="23">
- <image source="../Props/Static/Trees/Model_01/Size_04.png" width="368" height="256"/>
-</tileset>
--- Assets/Maps/Tileset - What Remains of You/Environment/Tilesets/Walls.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Walls" tilewidth="16" tileheight="16" tilecount="2100" columns="42">
- <image source="../Structures/Buildings/Walls.png" width="672" height="800"/>
-</tileset>
