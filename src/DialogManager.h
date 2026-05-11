f1dea14e99b93736c7c52e59b30c863d747db5e5
sofia-221b <sofia.bar.nav@gmail.com>
2026-05-11 21:30:12 +0200

    implementation of answer dialog texture ueeeeeeeeeeeeeeeeeeeeee finallyyyyyyyyy

--- src/DialogManager.h
+++ src/DialogManager.h
@@ -47,6 +47,7 @@ private:
 
 	DialogTree* currentDialog;
 	SDL_Texture* dialogBox;
+	SDL_Texture* answerBox;
 	std::shared_ptr<UILabel> dialogText;
 	std::shared_ptr<UILabel> speakerName;
 	std::shared_ptr<UIButton> answer1;
