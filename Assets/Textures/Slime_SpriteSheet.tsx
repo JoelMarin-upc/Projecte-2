7c29319f3e6d6a32932a6a1d3db005227bad32fc
Valkyn22 <clara.sanchez.via@gmail.com>
2026-06-01 13:47:30 +0200

    Chancged animations speeds

--- Assets/Textures/Slime_SpriteSheet.tsx
+++ Assets/Textures/Slime_SpriteSheet.tsx
@@ -3,42 +3,42 @@
  <image source="SpriteSheet_Slime.png" width="256" height="384"/>
  <tile id="0">
   <animation>
-   <frame tileid="0" duration="250"/>
-   <frame tileid="1" duration="250"/>
-   <frame tileid="2" duration="250"/>
-   <frame tileid="3" duration="250"/>
-   <frame tileid="4" duration="250"/>
-   <frame tileid="5" duration="250"/>
-   <frame tileid="6" duration="250"/>
-   <frame tileid="7" duration="250"/>
-   <frame tileid="8" duration="250"/>
-   <frame tileid="9" duration="250"/>
-   <frame tileid="10" duration="250"/>
-   <frame tileid="11" duration="250"/>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="500"/>
+   <frame tileid="4" duration="500"/>
+   <frame tileid="5" duration="500"/>
+   <frame tileid="6" duration="500"/>
+   <frame tileid="7" duration="500"/>
+   <frame tileid="8" duration="500"/>
+   <frame tileid="9" duration="500"/>
+   <frame tileid="10" duration="500"/>
+   <frame tileid="11" duration="500"/>
   </animation>
  </tile>
  <tile id="12">
   <animation>
-   <frame tileid="12" duration="200"/>
-   <frame tileid="13" duration="200"/>
-   <frame tileid="14" duration="200"/>
-   <frame tileid="15" duration="200"/>
+   <frame tileid="12" duration="300"/>
+   <frame tileid="13" duration="300"/>
+   <frame tileid="14" duration="300"/>
+   <frame tileid="15" duration="300"/>
   </animation>
  </tile>
  <tile id="16">
   <animation>
-   <frame tileid="16" duration="200"/>
-   <frame tileid="17" duration="200"/>
-   <frame tileid="18" duration="200"/>
-   <frame tileid="19" duration="200"/>
+   <frame tileid="16" duration="300"/>
+   <frame tileid="17" duration="300"/>
+   <frame tileid="18" duration="300"/>
+   <frame tileid="19" duration="300"/>
   </animation>
  </tile>
  <tile id="20">
   <animation>
-   <frame tileid="20" duration="250"/>
-   <frame tileid="21" duration="250"/>
-   <frame tileid="22" duration="250"/>
-   <frame tileid="23" duration="250"/>
+   <frame tileid="20" duration="300"/>
+   <frame tileid="21" duration="300"/>
+   <frame tileid="22" duration="300"/>
+   <frame tileid="23" duration="300"/>
   </animation>
  </tile>
 </tileset>
