8f1100803efa3c6be559b1784939c7e6500fb1af
Valkyn22 <clara.sanchez.via@gmail.com>
2026-06-05 18:35:03 +0200

    Adjust animations duration for infected characters

--- Assets/Textures/Garnik_Infected_SpriteSheet.tsx
+++ Assets/Textures/Garnik_Infected_SpriteSheet.tsx
@@ -3,34 +3,34 @@
  <image source="SpriteSheet_Garnik_Infected.png" width="128" height="256"/>
  <tile id="0">
   <animation>
-   <frame tileid="0" duration="300"/>
-   <frame tileid="1" duration="300"/>
-   <frame tileid="2" duration="300"/>
-   <frame tileid="3" duration="300"/>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="500"/>
   </animation>
  </tile>
  <tile id="4">
   <animation>
-   <frame tileid="4" duration="150"/>
-   <frame tileid="5" duration="150"/>
-   <frame tileid="6" duration="150"/>
-   <frame tileid="7" duration="150"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+   <frame tileid="6" duration="300"/>
+   <frame tileid="7" duration="300"/>
   </animation>
  </tile>
  <tile id="8">
   <animation>
-   <frame tileid="8" duration="150"/>
-   <frame tileid="9" duration="150"/>
-   <frame tileid="10" duration="150"/>
-   <frame tileid="11" duration="150"/>
+   <frame tileid="8" duration="300"/>
+   <frame tileid="9" duration="300"/>
+   <frame tileid="10" duration="300"/>
+   <frame tileid="11" duration="300"/>
   </animation>
  </tile>
  <tile id="12">
   <animation>
-   <frame tileid="12" duration="150"/>
-   <frame tileid="13" duration="150"/>
-   <frame tileid="14" duration="150"/>
-   <frame tileid="15" duration="150"/>
+   <frame tileid="12" duration="300"/>
+   <frame tileid="13" duration="300"/>
+   <frame tileid="14" duration="300"/>
+   <frame tileid="15" duration="300"/>
   </animation>
  </tile>
 </tileset>
--- Assets/Textures/Medic_Infected_SpriteSheet.tsx
+++ Assets/Textures/Medic_Infected_SpriteSheet.tsx
@@ -3,46 +3,46 @@
  <image source="SpriteSheet_Medic_Infected.png" width="512" height="256"/>
  <tile id="0">
   <animation>
-   <frame tileid="0" duration="300"/>
-   <frame tileid="1" duration="300"/>
-   <frame tileid="2" duration="300"/>
-   <frame tileid="3" duration="300"/>
-   <frame tileid="4" duration="300"/>
-   <frame tileid="5" duration="300"/>
-   <frame tileid="6" duration="300"/>
-   <frame tileid="7" duration="300"/>
-   <frame tileid="8" duration="300"/>
-   <frame tileid="9" duration="300"/>
-   <frame tileid="10" duration="300"/>
-   <frame tileid="11" duration="300"/>
-   <frame tileid="12" duration="300"/>
-   <frame tileid="13" duration="300"/>
-   <frame tileid="14" duration="300"/>
-   <frame tileid="15" duration="300"/>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="500"/>
+   <frame tileid="4" duration="500"/>
+   <frame tileid="5" duration="500"/>
+   <frame tileid="6" duration="500"/>
+   <frame tileid="7" duration="500"/>
+   <frame tileid="8" duration="500"/>
+   <frame tileid="9" duration="500"/>
+   <frame tileid="10" duration="500"/>
+   <frame tileid="11" duration="500"/>
+   <frame tileid="12" duration="500"/>
+   <frame tileid="13" duration="500"/>
+   <frame tileid="14" duration="500"/>
+   <frame tileid="15" duration="500"/>
   </animation>
  </tile>
  <tile id="16">
   <animation>
-   <frame tileid="16" duration="150"/>
-   <frame tileid="17" duration="150"/>
-   <frame tileid="18" duration="150"/>
-   <frame tileid="19" duration="150"/>
+   <frame tileid="16" duration="300"/>
+   <frame tileid="17" duration="300"/>
+   <frame tileid="18" duration="300"/>
+   <frame tileid="19" duration="300"/>
   </animation>
  </tile>
  <tile id="32">
   <animation>
-   <frame tileid="32" duration="150"/>
-   <frame tileid="33" duration="150"/>
-   <frame tileid="34" duration="150"/>
-   <frame tileid="35" duration="150"/>
+   <frame tileid="32" duration="300"/>
+   <frame tileid="33" duration="300"/>
+   <frame tileid="34" duration="300"/>
+   <frame tileid="35" duration="300"/>
   </animation>
  </tile>
  <tile id="48">
   <animation>
-   <frame tileid="48" duration="150"/>
-   <frame tileid="49" duration="150"/>
-   <frame tileid="50" duration="150"/>
-   <frame tileid="51" duration="150"/>
+   <frame tileid="48" duration="300"/>
+   <frame tileid="49" duration="300"/>
+   <frame tileid="50" duration="300"/>
+   <frame tileid="51" duration="300"/>
   </animation>
  </tile>
 </tileset>
